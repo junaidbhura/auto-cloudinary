baccba88302af713402d6d696013ce71bd6af54c
Junaid Bhura <info@junaidbhura.com>
2017-11-12 20:22:49 +1030

    Refactor

---
+++ inc/class-admin.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace JB\Cloudinary;
+
+class Admin {
+
+	private static $_instance = null;
+
+	/**
+	 * Get current instance.
+	 *
+	 * @return object
+	 */
+	public static function get_instance() {
+		if ( ! self::$_instance ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * Setup hooks and filters.
+	 *
+	 * @return void
+	 */
+	public function setup() {
+		add_action( 'admin_menu', array( $this, 'admin_menu_item' ) );
+	}
+
+	/**
+	 * Add admin menu item.
+	 *
+	 * @return void
+	 */
+	public function admin_menu_item() {
+		add_management_page(
+			__( 'Cloudinary', 'cloudinary' ),
+			__( 'Cloudinary', 'cloudinary' ),
+			apply_filters( 'cloudinary_user_capability', 'manage_options' ),
+			'auto-cloudinary',
+			array( $this, 'options_page' )
+		);
+	}
+
+	/**
+	 * Options page.
+	 *
+	 * @return void
+	 */
+	public function options_page() {
+		// Check for POST.
+		if (
+			isset( $_POST['cloudinary_nonce'] ) // Input var okay.
+			&& wp_verify_nonce( sanitize_key( $_POST['cloudinary_nonce'] ), 'cloudinary_options' ) // Input var okay.
+		) {
+			update_option( 'cloudinary_cloud_name', sanitize_text_field( $_POST['cloudinary_cloud_name'] ) );
+			update_option( 'cloudinary_auto_mapping_folder', sanitize_text_field( $_POST['cloudinary_auto_mapping_folder'] ) );
+			$urls = trim( sanitize_textarea_field( $_POST['cloudinary_urls'] ) );
+			if ( empty( $urls ) ) {
+				$urls = 'https://res.cloudinary.com';
+			}
+			update_option( 'cloudinary_urls', $urls );
+			if ( empty( $_POST['cloudinary_content_images'] ) ) {
+				$content_images = '';
+			} else {
+				$content_images = sanitize_text_field( $_POST['cloudinary_content_images'] );
+			}
+			update_option( 'cloudinary_content_images', $content_images );
+
+			echo '<div class="updated"><p>' . esc_html__( 'Options saved.', 'fly-images' ) . '</p></div>';
+		}
+
+		// Load template.
+		load_template( JB_CLOUDINARY_PATH . '/admin/options.php' );
+	}
+
+}
---
+++ inc/class-frontend.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace JB\Cloudinary;
+
+
+class Frontend {
+
+	private static $_instance = null;
+
+	/**
+	 * Get current instance.
+	 *
+	 * @return object
+	 */
+	public static function get_instance() {
+		if ( ! self::$_instance ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * Setup hooks and filters.
+	 *
+	 * @return void
+	 */
+	public function setup() {
+		// Check if we need to replace content images on the front-end.
+		$replace_content = false;
+		if ( '1' === get_option( 'cloudinary_content_images' ) && apply_filters( 'cloudinary_content_images', true ) ) {
+			$replace_content = true;
+		}
+
+		if ( apply_filters( 'cloudinary_filter_the_content', $replace_content ) ) {
+			add_filter( 'the_content', 'cloudinary_update_content_images', 999 );
+		}
+		if ( apply_filters( 'cloudinary_filter_wp_get_attachment_url', $replace_content ) ) {
+			add_filter( 'wp_get_attachment_url', array( $this, 'filter_wp_get_attachment_url' ), 999, 2 );
+		}
+		if ( apply_filters( 'cloudinary_filter_image_downsize', $replace_content ) ) {
+			add_filter( 'image_downsize', array( $this, 'filter_image_downsize' ), 999, 3 );
+		}
+		if ( apply_filters( 'cloudinary_filter_wp_calculate_image_srcset', $replace_content ) ) {
+			add_filter( 'wp_calculate_image_srcset', array( $this, 'filter_wp_calculate_image_srcset' ), 999, 5 );
+		}
+	}
+
+	/**
+	 * Filter wp_get_attachment_url to use Cloudinary.
+	 *
+	 * @param  string $url
+	 * @param  int    $post_id
+	 * @return string
+	 */
+	public function filter_wp_get_attachment_url( $url, $post_id ) {
+		if ( ! apply_filters( 'cloudinary_ignore', false ) ) {
+			return cloudinary_url( $url );
+		}
+		return $url;
+	}
+
+	/**
+	 * Filter image_downsize to use Cloudinary.
+	 *
+	 * @param  bool  $downsize
+	 * @param  int   $id
+	 * @param  mixed $size
+	 * @return array|bool
+	 */
+	public function filter_image_downsize( $downsize, $id, $size ) {
+		if ( 'full' === $size || is_array( $size ) || apply_filters( 'cloudinary_ignore', false ) ) {
+			return false;
+		}
+
+		$dimensions = $this->get_image_size( $size );
+		if ( empty( $dimensions ) ) {
+			return false;
+		}
+
+		return array(
+			cloudinary_url( $id, array(
+				'transform' => array(
+					'width'  => $dimensions['width'],
+					'height' => $dimensions['height'],
+				),
+			) ),
+			$dimensions['width'],
+			$dimensions['height'],
+			true, // Is intermediate.
+		);
+	}
+
+	/**
+	 * Filter wp_calculate_image_srcset to use Cloudinary.
+	 *
+	 * @param  array  $sources
+	 * @param  array  $size_array
+	 * @param  string $image_src
+	 * @param  array  $image_meta
+	 * @param  int    $attachment_id
+	 * @return array
+	 */
+	public function filter_wp_calculate_image_srcset( $sources, $size_array, $image_src, $image_meta, $attachment_id ) {
+		if ( ! apply_filters( 'cloudinary_ignore', false ) ) {
+			if ( ! empty( $sources ) ) {
+				$original_url = cloudinary_get_original_url( $attachment_id );
+				foreach ( $sources as $key => $source ) {
+					$dimensions = $this->get_srcset_dimensions( $image_meta, $source );
+					$transform  = array();
+					if ( ! empty( $dimensions ) ) {
+						$transform = array(
+							'transform' => $dimensions,
+						);
+					}
+					$transform = apply_filters( 'cloudinary_image_srcset_transform', $transform, $original_url, $attachment_id );
+
+					if ( ! empty( $transform ) ) {
+						$sources[ $key ]['url'] = cloudinary_url( $original_url, $transform );
+					}
+				}
+			}
+		}
+		return $sources;
+	}
+
+	/**
+	 * Get dimensions from image meta which matches a descriptor.
+	 *
+	 * @param  array $image_meta
+	 * @param  array $source
+	 * @return array
+	 */
+	public function get_srcset_dimensions( $image_meta = array(), $source = array() ) {
+		$dimension = 'w' === $source['descriptor'] ? 'width' : 'height';
+		foreach ( $image_meta['sizes'] as $key => $size ) {
+			if ( $size[ $dimension ] === $source['value'] ) {
+				return array(
+					'width'  => $size['width'],
+					'height' => $size['height'],
+				);
+			}
+		}
+		return array(
+			$dimension => $source['value'],
+		);
+	}
+
+	/**
+	 * Get size information for all currently-registered image sizes.
+	 *
+	 * @global $_wp_additional_image_sizes
+	 * @uses   get_intermediate_image_sizes()
+	 * @return array $sizes Data for all currently-registered image sizes.
+	 *
+	 * @see    https://codex.wordpress.org/Function_Reference/get_intermediate_image_sizes
+	 */
+	public function get_image_sizes() {
+		global $_wp_additional_image_sizes;
+
+		$sizes = array();
+
+		foreach ( get_intermediate_image_sizes() as $_size ) {
+			if ( in_array( $_size, array( 'thumbnail', 'medium', 'medium_large', 'large' ) ) ) {
+				$sizes[ $_size ]['width']  = get_option( "{$_size}_size_w" );
+				$sizes[ $_size ]['height'] = get_option( "{$_size}_size_h" );
+				$sizes[ $_size ]['crop']   = (bool) get_option( "{$_size}_crop" );
+			} elseif ( isset( $_wp_additional_image_sizes[ $_size ] ) ) {
+				$sizes[ $_size ] = array(
+					'width'  => $_wp_additional_image_sizes[ $_size ]['width'],
+					'height' => $_wp_additional_image_sizes[ $_size ]['height'],
+					'crop'   => $_wp_additional_image_sizes[ $_size ]['crop'],
+				);
+			}
+		}
+
+		return $sizes;
+	}
+
+	/**
+	 * Get size information for a specific image size.
+	 *
+	 * @uses   get_image_sizes()
+	 * @param  string $size The image size for which to retrieve data.
+	 * @return bool|array $size Size data about an image size or false if the size doesn't exist.
+	 *
+	 * @see    https://codex.wordpress.org/Function_Reference/get_intermediate_image_sizes
+	 */
+	public function get_image_size( $size ) {
+		$sizes = $this->get_image_sizes();
+
+		if ( ! empty( $sizes ) && isset( $sizes[ $size ] ) ) {
+			return $sizes[ $size ];
+		}
+
+		return false;
+	}
+
+}
